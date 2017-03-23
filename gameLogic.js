e6554adfb66e7f8010d8545aa0071a3106c67d3f
Dan Nevarez <dnev.dev@gmail.com>
2017-03-23 15:12:03 -0600

    complete singleton

--- gameLogic.js
+++ gameLogic.js
@@ -164,126 +164,329 @@ var Sudoku = ( function ( $ ){
        return $table
      },
 
-    };
+   /**
+     * Handle Keyup events.
+     *
+     * @param {jQuery.event} e Keyup event
+     */
+     onKeyUp: function( e ) {
+       var sectRow, sectCol, secIndex,
+           starttime, endtime, elapsed,
+           isValid = true;
+           val = $.trim( $( e.currentTarget ).val() ),
+           row = $( e.currentTarget ).data( 'row' ),
+           col = $( e.currentTarget ).data( 'col' );
 
-    // helper functions
+       // Reset board validation class
+       $( '.sudoku-container' ).removeClass( 'valid-matrix' );
 
-    // Singleton public Methods
-    return {
-      // Get the Singleton instance
-      getInstance: function( config ) {
-        if (!_instance) {
-          _instance = init (config);
-        }
-        return _instance;
+       // Validate, but inly if validate_on_insert is set to true
+       if ( this.config.validate_on_insert ) {
+           isValid = this.validateNumber( val, row, col, this.matrix.row[row][col] );
+           // Indicate error
+           $( e.currentTarget ).toggleClass( 'sudoku-input-error', !isValid );
+       }
+
+       // Calculate section identifiers
+       sectRow = Math.floor(row / 3);
+       sectCol = Math.floor(col / 3);
+       sectIndex = ( row % 3 ) * 3 + ( col % 3 );
+
+       // Cache value in matrix
+       this.matrix.row[row][col] = val;
+       this.matrix.col[col][row] = val;
+       this.matrix.sect[sectRow][sectCol][sectIndex] = val;
+     },
+
+     /**
+      * Reset the board and the game parameters
+      */
+     resetGame: function(){
+          this.resetValidationMatrices();
+          for ( var row = 0; row < 9; row++ ){
+            for ( var col = 0; col < 9; col++ ){
+                // Reset GUI inputs
+                this.$cellMatrix[row][col].val( '' );
+            }
+          }
+
+          $( '.sudoku-container input' ).removeAttr( 'disabled' );
+          $( '.sudoku-container' ).removeClass( 'valid-matrix' );
+     },
+
+     /**
+      * Reset and rebuild the validation matrices
+      */
+     resetValidationMatrices: function() {
+          this.matrix = { 'row': {}, 'col': {}, 'sect': {} };
+          this.validation = { 'row': {}, 'col': {}, 'sect': {} };
+
+          // Build the row/col matrix and validation arrays
+          for ( var i = 0; i < 9; i++ ) {
+              this.matrix.row[i] = [ '', '', '', '', '', '', '', '', '' ];
+              this.matrix.col[i] = [ '', '', '', '', '', '', '', '', '' ];
+              this.validation.row[i] = [];
+              this.validation.col[i] = [];
+          }
+
+          // Build the section matrix and validation arrays
+          for ( var row = 0; row < 3; row++ ) {
+                this.matrix.sect[row] = [];
+                this.validation.sect[row] = {};
+                for ( var col = 0; col < 3; col++ ) {
+                    this.matrix.sect[row][col] = [ '', '', '', '', '', '', '', '', '' ];
+                    this.validation.sect[row][col] = [];
+                }
+          }
+     },
+
+     /**
+      * Validate the current number inserted.
+      *
+      * @param {String} num The value that is inserted
+      * @param {Number} rowID The row the number belongs to
+      * @param {Number} colID The column the number belongs to
+      * @param {String} oldNum The previous value
+      * @returns {Boolean} Valid or invalid input
+      */
+     validateNumber: function( num, rowID, colID, oldNum ) {
+         var isValid: = true,
+           // Section
+           sectRow = Math.floor( rowID / 3 ),
+           sectCol = Math.floor( colID / 3 );
+
+         // This is given as the matrix component (old value in
+         // case of change to the input) in the case of on-insert
+         // validation. However, in the solver, validating the
+         // old number is unnecessary.
+         oldNum = oldNum || '';
+
+         // Remove oldNum from the validation matrices,
+         // if it exists in them.
+         if ( this.validation.row[rowID].indexOf( oldNum ) > -1 ){
+            this.validation.row[rowID].splice(
+                this.validation.row[rowID].indexOf( oldNum ), 1
+              );
+         }
+         if ( this.validation.col[colID].indexOf( oldNum ) > -1 ){
+            this.validation.col[colID].splice(
+                this.validation.col[colID].indexOf( oldNum ), 1
+              );
+         }
+         if ( this.validation.sect[sectRow][sectCol].indexOf( oldNum ) > -1 ) {
+              this.validation.sect[sectRow][sectCol].splice(
+                this.validation.sect[sectRow][sectCol].indexOf( oldNum ), 1
+              );
+         }
+         // Skip if empty value
+         if ( num !== '' ) {
+           // Validate value
+           if (
+             // Make sure value is numeric
+             $.isNumeric( num ) &&
+             // Make sure value is within range
+             Number( num ) > 0 &&
+             Number( num ) <= 9
+           ) {
+             // Check if it already exists in validation array
+             if (
+               $.inArray( num, this.validation.row[rowID] ) > -1 ||
+               $.inArray( num, this.validation.col[colID] ) > -1 ||
+               $.inArray( num, this.validation.sect[sectRow][sectCol] ) > -1
+             ) {
+               isValid = false;
+             } else {
+               isValid = true;
+             }
+           }
+
+           // Insert new value into validation array even if it isn't
+           // valid. This is on purpose: If there are two numbers in the same
+           // row/col/section and one is replaced, the other still
+           // exists and should be reflected in the validation.
+           // The validation will keep records of dupicates so it can
+           // remove them safely when validating later changes.
+           this.validation.row[rowID].push( num );
+           this.validation.col[colID].push( num );
+           this.validation.sect[sectRow][sectCol].push( num )
+         }
+         return isValid;
+     },
+
+		/**
+		 * Validate the entire matrix
+		 * @returns {Boolean} Valid or invalid matrix
+		 */
+    validateMatrix: function() {
+          var isValid, val, $element,
+                hasError = false;
+
+          // Go over entire board, and compare to the cached
+          // validation arrays
+          for ( var row = 0; row < 9; row++ ) {
+              for ( var col = 0; col < 9; col++ ) {
+                    val = this.matrix.row[row][col];
+                    // Validate the value
+                    isValid = this.validateNumber( val, row, col, val );
+                    this.$cellMatrix[row][col].toggleClass( 'sudoku-input-error', !isValid );
+                    this ( !isValid ) {
+                          hasError = true;
+                    }
+              }
+          }
+          return !hasError;
+    },
+
+    /**
+     * A recursive 'backtrack' solver for the
+     * game. Algorithm is based on the StackOverflow answer
+     * http://stackoverflow.com/questions/18168503/recursively-solving-a-sudoku-puzzle-using-backtracking-theoretically
+     */
+    solveGame: function( row, col ) {
+      var cval, sqRow, sqCol, $nextSquare, legalValues,
+            sectRow, sectCol, secIndex, gameResult;
+
+          this.recursionCounter++;
+          $nextSquare = this.findClosestEmptySquare( row, col );
+          if ( !$nextSquare ) {
+              // End of board
+              return true;
+          } else {
+                sqRow = $nextSquare.data( 'row' );
+                sqCol = $nextSquare.data( 'col' );
+                legalValues = ths.findLegalValuesForSquare( sqRow, sqCol );
+
+                // Find the segment id
+                sectRow = Math.floor( sqRow / 3 );
+                sectCol = Math.floor( sqCol / 3 );
+                sectIndex = ( sqRow % 3 ) * 3 + ( sqCol % 3 );
+
+                // Try out legal values for this cell
+                for ( var i = 0; i < legalValues.length; i++ ) {
+                      cval = legalValues[i];
+                      // Update value in input
+                      $nextSquare.val( cval );
+                      // Update in matrices
+                      this.matrix.row[sqRow][sqCol] = cval;
+                      this.matrix.col[sqCol][sqRow] = cval;
+                      this.matrix.sect[sectCol][sectRow] = cval;
+
+                      // Recursively keep trying
+                      if ( this.solveGame( sqRow, sqCol ) ) {
+                            return true;
+                      } else {
+                            // There was a problem, we should backtrack
+                            this.backtrackCounter++;
+
+                            // Remove value from input
+                            this.$cellMatrix[sqRow][sqCol].val( '' );
+                            // Remove value form matrices
+                            this.matrix.row[sqRow][sqCol] = '';
+                						this.matrix.col[sqCol][sqRow] = '';
+                						this.matrix.sect[sectRow][sectCol][secIndex] = '';
+                      }
+                }
+                // If there was no success with any of the legal
+                // numbers, call bactrack recursivley backwards
+                return false;
+          }
+    },
+
+    /**
+     * Find closest empty square relative to the given cell.
+     *
+     * @param {Number} row Row id
+     * @param {Number} col Column id
+     * @returns {jQuery} Input element of the closest empty
+     *  square
+     */
+     findClosestEmptySquare: function( row, col ) {
+          var walkingRow, walkingCol, found = false;
+          for ( var i = ( col + 9 * row ); i < 81; i++ ) {
+              walkingRow = Math.floor( i / 9 );
+              walkingCol = i % 9;
+              if ( this.matrix.row[walkingRow][walkingCol] === '' ) {
+                    found = true;
+                    return this.$cellMatrix[walkingRow][walkingCol];
+              }
+          }
+     },
+
+     /**
+      * Find the available legal numbers for the square in the
+      * given row and column.
+      *
+      * @param {Number} row Row id
+      * @param {Number} col Column id
+      * @returns {Array} An array of available numbers
+      */
+     findLegalValuesForSquare: function( row, col ) {
+          var legalVals, legalNums, val, i,
+                sectRow = Math.floor( row / 3 ),
+                sectCol = Math.floor( col / 3 );
+
+              legalNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+
+              // Check existing number in col
+              for ( i = 0; i < 9; i++ ) {
+                  val = Number( this.matrix.col[col][i] );
+                  if ( val > 0 ) {
+                      // Remove from array
+                      if ( legalNums.indexOf( val ) > -1 ) {
+                              legalNums.splice( legalNums.indexOf( val ), 1 );
+                      }
+                  }
+              }
+
+              // Check existing number in section
+              sectRow = Math.floor( row / 3 );
+              sectCol = Math.floor( col / 3 );
+              for ( i = 0; i < 9; i++ ) {
+                    val = Number( this.matrix.sect[sectRow][sectCol][i] );
+                    if ( val > 0 ) {
+                      // Remove from array
+                      if ( legalNums.indexOf( val ) > -1 ) {
+                              legalNums.splice( legalNums.indexOf( val ), 1 );
+                      }
+                    }
+              }
+
+              if ( this.config.solver_shuffle_numbers ) {
+                  // Shuffling the resulting 'legalNums' array will
+                  // make sure the solver produces different answers
+                  // for the same scenario. Otherwise, 'legalNums'
+                  // will be chosen in sequence.
+                  for ( i = legalNums.length - 1; i > 0; i-- ) {
+                          var rand = getRandomInt( 0, i );
+                          temp = legalNums[i];
+                          legalNums[i] = legalNums[rand];
+                          legalNums[rand] = temp;
+                  }
+              }
+
+              return legalNums;
+     },
+
+  };
+
+  /**
+	 * Get a random integer within a range
+	 *
+	 * @param {Number} min Minimum number
+	 * @param {Number} max Maximum range
+	 * @returns {Number} Random number within the range (Inclusive)
+	 */
+   function getRandomInt(min, max) {
+        return Math.floor( Math.random() * ( max + 1 ) ) + min;
+   }
+
+  return {
+    // Get the Singleton instance
+    getInstance: function( config ) {
+      if (!_instance) {
+        _instance = init (config);
       }
+      return _instance;
     }
+  };
 } ) ( jQuery );
-
-/**
-  * Handle Keyup events.
-  *
-  * @param {jQuery.event} e Keyup event
-  */
-
-onKeyUp: function( e ) {
-  var sectRow, sectCol, secIndex,
-      starttime, endtime, elapsed,
-      isValid = true;
-      val = $.trim( $( e.currentTarget ).val() ),
-      row = $( e.currentTarget ).data( 'row' ),
-      col = $( e.currentTarget ).data( 'col' );
-
-  // Reset board validation class
-  $( '.sudoku-container' ).removeClass( 'valid-matrix' );
-
-  // Validate, but inly if validate_on_insert is set to true
-  if ( this.config.validate_on_insert ) {
-      isValid = this.validateNumber( val, row, col, this.matrix.row[row][col] );
-      // Indicate error
-      $( e.currentTarget ).toggleClass( 'sudoku-input-error', !isValid );
-  }
-
-  // Calculate section identifiers
-  sectRow = Math.floor(row / 3);
-  sectCol = Math.floor(col / 3);
-  sectIndex = ( row % 3 ) * 3 + ( col % 3 );
-
-  // Cache value in matrix
-  this.matrix.row[row][col] = val;
-  this.matrix.col[col][row] = val;
-  this.matrix.sect[sectRow][sectCol][sectIndex] = val;
-}
-
-/**
- * Validate the current number inserted.
- *
- * @param {String} num The value that is inserted
- * @param {Number} rowID The row the number belongs to
- * @param {Number} colID The column the number belongs to
- * @param {String} oldNum The previous value
- * @returns {Boolean} Valid or invalid input
- */
-validateNumber: function( num, rowID, colID, oldNum ) {
-    var isValid: = true,
-      // Section
-      sectRow = Math.floor( rowID / 3 ),
-      sectCol = Math.floor( colID / 3 );
-
-    // This is given as the matrix component (old value in
-    // case of change to the input) in the case of on-insert
-    // validation. However, in the solver, validating the
-    // old number is unnecessary.
-    oldNum = oldNum || '';
-
-    // Remove oldNum from the validation matrices,
-    // if it exists in them.
-    if ( this.validation.row[rowID].indexOf( oldNum ) > -1 ){
-      this.validation.row[rowID].splice(
-         this.validation.row[rowID].indexOf( oldNum ), 1
-      );
-    }
-    if ( this.validation.col[colID].indexOf( oldNum ) > -1 ){
-      this.validation.col[colID].splice(
-         this.validation.col[colID].indexOf( oldNum ), 1
-      );
-    }
-    if ( this.validation.sect[sectRow][sectCol].indexOf( oldNum ) > -1 ) {
-        this.validation.sect[sectRow][sectCol].splice(
-          this.validation.sect[sectRow][sectCol].indexOf( oldNum ), 1
-        );
-    }
-    // Skip if empty value
-    if ( num !== '' ) {
-      // Validate value
-      if (
-        // Make sure value is numeric
-        $.isNumeric( num ) &&
-        // Make sure value is within range
-        Number( num ) > 0 &&
-        Number( num ) <= 9
-      ) {
-        // Check if it already exists in validation array
-        if (
-          $.inArray( num, this.validation.row[rowID] ) > -1 ||
-          $.inArray( num, this.validation.col[colID] ) > -1 ||
-          $.inArray( num, this.validation.sect[sectRow][sectCol] ) > -1
-        ) {
-          isValid = false;
-        } else {
-          isValid = true;
-        }
-      }
-
-      // Insert new value into validation array even if it isn't
-      // valid. This is on purpose: If there are two numbers in the same
-      // row/col/section and one is replaced, the other still
-      // exists and should be reflected in the validation.
-      // The validation will keep records of dupicates so it can
-      // remove them safely when validating later changes.
-      this.validation.row[rowID].push( num );
-      this.validation.col[colID].push( num );
-      this.validation.sect[sectRow][sectCol].push( num )
-    }
-    return isValid;
-}
